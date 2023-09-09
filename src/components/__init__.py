de4a3f95cd51a6478245fb57ef42f4d6a68d4c8f
Nidhi <nid200128@gmail.com>
2023-09-09 18:22:38 +0530

    logging and exception

---
+++ src/components/__init__.py
@@ -0,0 +1,3 @@
+# Components can be created as a package and it can also be exported to some other file location.That is why
+# __intit.py file is made in components.
+#components are basically all the modules that we'll create 
